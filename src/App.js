79ffefad370c3b70a8e79db79030ae456a4c1a8c
Alhussain <omarh3795@gmail.com>
2021-09-22 13:49:07 +0200

    adjusting customhook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState , useCallback } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
@@ -6,7 +6,7 @@ import useHttp from "./hooks/use-http";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const transformTasks = (taskObj) => {
+  const transformTasks = useCallback( (taskObj) => {
     const loadedTasks = [];
 
     for (const taskKey in taskObj) {
@@ -14,15 +14,13 @@ function App() {
     }
 
     setTasks(loadedTasks);
-  };
-    const {isLoading , error , sendRequest : fetchTasks} = useHttp({
-    url: "https://myfirst-8b245-default-rtdb.firebaseio.com/tasks.json",
-  },transformTasks);
+  },[]);
+    const {isLoading , error , sendRequest : fetchTasks} = useHttp(transformTasks);
    
   
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({url: "https://myfirst-8b245-default-rtdb.firebaseio.com/tasks.json"});
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
